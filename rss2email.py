abba1aeb3ee4db2d2aa917a6ecd2b99ca560259d
tzwm <wangjunzhe4444@gmail.com>
2013-07-18 15:44:24 +0800

    Refactor and support multi-item

--- rss2email.py
+++ rss2email.py
@@ -13,11 +13,9 @@ from hashlib import md5
 SMTP_SERVER = 'smtp.gmail.com'
 SMTP_PORT = 587
  
-sender = "" 
-password = "" 
-recipient = 'tzwm.rss@gmail.com'
-
-lastMD5 = ""
+SENDER = "tzwm.rss@gmail.com" 
+PASSWORD = "" 
+RECIPIENT = 'tzwm.rss@gmail.com'
 
 def getMD5(s):
     m = md5()
@@ -25,77 +23,109 @@ def getMD5(s):
 
     return m.hexdigest()
 
+class FeedItem():
+    feedLink = ""
+    lastMD5 = ""
+    lastModified = ""
+    feedD = ""
+    infoLine = ""
 
-def checkItemUpdate(item):
-    itemStr = item.published
-    itemStr += item.link
-    md5Str = getMD5(itemStr)
+    session = ""
 
-    return md5Str == lastMD5
+    def __init__(self, inputLine, _session):
+        self.session = _session
+        self.infoLine = inputLine
+
+        inputLine = inputLine.split(' ', 2)
+        self.feedLink = inputLine[0]
+        if len(inputLine) == 1:
+            self.feedD = feedparser.parse(self.feedLink)
+        else:
+            self.lastMD5 = inputLine[1]
+            self.lastModified = inputLine[2]
+            self.feedD = feedparser.parse(self.feedLink, modified=self.lastModified)
+
+    def checkUpdate(self):
+        if self.feedD.status == 304:
+            return False 
+        else:
+            return True
+
+    def checkItem(self, item):
+        itemStr = item.published
+        itemStr += item.link
+        md5Str = getMD5(itemStr)
+    
+        return md5Str == self.lastMD5
+
+    def sendItem(self, item):
+        msg = MIMEMultipart()
+        msg['From'] = SENDER
+        msg['To'] = RECIPIENT
+        message = item.description + '\n' + item.link
+        msg['Subject'] = item.title
+        msg.attach(MIMEText(message.encode('utf8'), 'html'))
+        #print(msg.as_string())
+        self.session.sendmail(SENDER, RECIPIENT, msg.as_string())
+
+    def saveStatus(self):
+        self.infoLine = self.feedLink + ' ' + self.lastMD5 + ' ' + self.feedD.modified + '\n'
+
+    def update(self):
+        if not self.checkUpdate():
+            return False
+
+        ret = 0;
+        if self.lastMD5 == "":
+            ret = 1
+    
+        for feed in reversed(self.feedD.entries):
+            if ret == 0:
+                if self.checkItem(feed):
+                    ret = 1
+                continue
+   
+            self.sendItem(feed)
+
+        if ret == 0:
+            for feed in reversed(self.feedD.entries):
+                self.sendItem(feed)
+
+        feed = self.feedD.entries[0]
+        self.lastMD5 = getMD5(feed.published + feed.link)
+        self.saveStatus()
+
+        return True
+
 
 def main():
     reload(sys)
     sys.setdefaultencoding('utf8')
 
-    sender = raw_input("sender is:")
-    password = raw_input("password of sender:")
-
+    lines = []
     o_file = open("rsslist.txt", "r")
-    line = o_file.readline()
+    for line in o_line:
+        lines.append(line.strip('\n'))
     o_file.close()
-
-    line = line.split(' ', 2)
-    rss_add = line[0]
-    global lastMD5
-    if len(line) < 2:
-        d = feedparser.parse(rss_add)
-    else:
-        lastMD5 = line[1]
-        rss_modified = line[2]
-        d = feedparser.parse(rss_add, modified=rss_modified)
-
-    
-    if d.status == 304:
-        print("no update")
-        return;
-
 
     session = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
     session.ehlo()
     session.starttls()
     session.ehlo
-    session.login(sender, password)
-
-    ret = 0;
-    if lastMD5 == "":
-        ret = 1
-
-    for feed in reversed(d.entries):
-    #for feed in d.entries:
-        if ret == 0:
-            if checkItemUpdate(feed):
-                ret = 1
-            continue
-
-        msg = MIMEMultipart()
-        msg['From'] = sender
-        msg['To'] = recipient
-        message = feed.description
-        msg['Subject'] = feed.title
-        msg.attach(MIMEText(message.encode('utf8'), 'html'))
-        #print(msg.as_string())
-        session.sendmail(sender, recipient, msg.as_string())
-
-    feed = d.entries[0]
-    lastMD5 = getMD5(feed.published + feed.link)
-    session.quit()
+    session.login(SENDER, PASSWORD)
 
     o_file = open("rsslist.txt", "w")
-    o_file.write(rss_add)
-    o_file.write(' ')
-    o_file.write(lastMD5)
-    o_file.write(' ')
-    o_file.write(d.modified)
+
+    for line in lines:
+        feedItem = FeedItem(line, session)
+        ret = feedItem.update()
+        o_file.write(feedItem.infoLine)
+        if(ret):
+            print("Update Finished")
+        else:
+            print("No Update")
+
+    session.quit()
     o_file.close()
 
 if __name__ == '__main__':
