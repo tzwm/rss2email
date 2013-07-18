4a87db90aa5af6e95a52f37783a8331dc80ec9aa
tzwm <wangjunzhe4444@gmail.com>
2013-07-18 16:26:15 +0800

    Fix bug about modified attribute at saveItem()

--- rss2email.py
+++ rss2email.py
@@ -40,7 +40,10 @@ class FeedItem():
         self.feedLink = inputLine[0]
         if len(inputLine) == 1:
             self.feedD = feedparser.parse(self.feedLink)
-        else:
+        if len(inputLine) == 2:
+            self.lastMD5 = inputLine[1]
+            self.feedD = feedparser.parse(self.feedLink)
+        if len(inputLine) == 3:
             self.lastMD5 = inputLine[1]
             self.lastModified = inputLine[2]
             self.feedD = feedparser.parse(self.feedLink, modified=self.lastModified)
@@ -69,7 +72,11 @@ class FeedItem():
         self.session.sendmail(SENDER, RECIPIENT, msg.as_string())
 
     def saveStatus(self):
-        self.infoLine = self.feedLink + ' ' + self.lastMD5 + ' ' + self.feedD.modified + '\n'
+        if hasattr(self.feedD, 'modified'):
+            self.lastModified = self.feedD.modified
+        else:
+            self.lastModified = ""
+        self.infoLine = self.feedLink + ' ' + self.lastMD5 + ' ' + self.lastModified + '\n'
 
     def update(self):
         if not self.checkUpdate():
