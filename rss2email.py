e0dd9c4e1465bb631a3efbfda2ba1f236cb33941
tzwm <wangjunzhe4444@gmail.com>
2013-07-19 15:17:00 +0800

    Add logging module

--- rss2email.py
+++ rss2email.py
@@ -1,14 +1,15 @@
 import os, re
 import sys
 import smtplib 
-
-import base.feedparser as feedparser
+import logging
+from hashlib import md5
 
 from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-from hashlib import md5
+import base.feedparser as feedparser
+import base.log
 
 SMTP_SERVER = 'smtp.gmail.com'
 SMTP_PORT = 587
@@ -32,7 +33,7 @@ class FeedItem():
 
     session = ""
 
-    def __init__(self, inputLine, _session):
+    def __init__(self, inputLine, _session, outputInfo):
         self.session = _session
         self.infoLine = inputLine
 
@@ -49,13 +50,14 @@ class FeedItem():
             self.lastModified = inputLine[2]
             self.feedD = feedparser.parse(self.feedLink, modified=self.lastModified)
 
-        print self.feedD.feed.title + ":",
+        logging.info(outputInfo + ' ' + self.feedD.feed.title + ':')
 
     def checkUpdate(self):
-        if self.feedD.status == 304:
-            return False 
-        else:
-            return True
+        #if self.feedD.status == 304:
+            #return False 
+        #else:
+            #return True
+        return True
 
     def checkItem(self, item):
         itemStr = item.published
@@ -64,15 +66,15 @@ class FeedItem():
     
         return md5Str == self.lastMD5
 
-    def sendItem(self, item):
+    def sendItem(self, item, number):
         msg = MIMEMultipart()
         msg['From'] = SENDER
         msg['To'] = RECIPIENT
         message = item.description + '<br /><br />' + item.link
         msg['Subject'] = item.title
         msg.attach(MIMEText(message.encode('utf8'), 'html'))
-        #print(msg.as_string())
         self.session.sendmail(SENDER, RECIPIENT, msg.as_string())
+        logging.info('%s: %s', str(number), item.title)
 
     def saveStatus(self):
         if hasattr(self.feedD, 'modified'):
@@ -97,38 +99,48 @@ class FeedItem():
                 continue
             
             number = number + 1
-            self.sendItem(feed)
+            self.sendItem(feed, number)
 
         if ret == 0:
             for feed in reversed(self.feedD.entries):
-                self.sendItem(feed)
+                self.sendItem(feed, number)
 
         feed = self.feedD.entries[0]
         self.lastMD5 = getMD5(feed.published + feed.link)
         self.saveStatus()
 
         if number == 0:
-            print "No Update"
+            logging.info("No Update.")
             return False
         else:
-            print "Update " + str(number) + " items"
+            logging.info("Updated %s items.", str(number))
             return True
 
 def main():
     reload(sys)
     sys.setdefaultencoding('utf8')
+    base.log.init()
+    logging.info('rss2email is running...')
 
     lines = []
     o_file = open("rsslist.txt", "r")
     for line in o_file:
+        if line.strip('\n') == '':
+            continue
         lines.append(line)
     o_file.close()
+    logging.info('rsslist got successful.')
 
     session = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
     session.ehlo()
     session.starttls()
     session.ehlo
-    session.login(SENDER, PASSWORD)
+    try:
+        session.login(SENDER, PASSWORD)
+    except Exception, data:
+        logging.error('Email Login Error.')
+        return
+    logging.info('Email Login Successful.')
 
     o_file = open("rsslist.txt", "w")
 
@@ -136,8 +148,7 @@ def main():
     for line in lines:
         num = num + 1
         outputStep = '(' + str(num) + '/' + str(len(lines)) + ')'
-        print outputStep,
-        feedItem = FeedItem(line, session)
+        feedItem = FeedItem(line, session, outputStep)
         ret = feedItem.update()
         o_file.write(feedItem.infoLine)
 
