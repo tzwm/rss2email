fccff4b77009824de41a00334e62699e161884cd
tzwm <wangjunzhe4444@gmail.com>
2013-09-04 23:40:22 +0800

    Modified the way to save the rsslist.xml

--- rss2email.py
+++ rss2email.py
@@ -29,15 +29,12 @@ class FeedItem():
     lastMD5 = ""
     lastModified = ""
 
-    def __init__(self, item, _node, _session, outputInfo, _sender, _recipient):
+    def __init__(self, item, outputInfo, _session, _sender, _recipient, _rsslist):
         self.session = _session
         self.sender = _sender
         self.recipient = _recipient
-        self.node = _node
-        self.node.set('text', item.attrib['text'])
-        self.node.set('type', item.attrib['type'])
-        self.node.set('htmlUrl', item.attrib['htmlUrl'])
-        self.node.set('rssUrl', item.attrib['rssUrl'])
+        self.rsslist = _rsslist
+        self.node = item 
         self.title = item.attrib['text']
         self.rssUrl = item.attrib['rssUrl']
         if 'lastMD5' in item.attrib.keys():
@@ -89,13 +86,12 @@ class FeedItem():
         self.session.sendmail(self.sender, self.recipient, msg.as_string())
         logging.info('%s: %s', str(number), item.title)
 
-    def saveStatus(self):
-        if hasattr(self.feedD, 'modified'):
-            self.lastModified = self.feedD.modified
-        else:
-            self.lastModified = ""
-        self.node.set('lastMD5', self.lastMD5)
-        self.node.set('lastModified', self.lastModified)
+    def saveXML(self):
+        xml_string = ET.tostring(self.rsslist.getroot())
+        doc = minidom.parseString(xml_string)
+        o_file = open('rsslist.xml', 'w')
+        o_file.write(doc.toprettyxml())
+        o_file.close()
 
     def update(self):
         if not self.checkFeedStatus(self.feedD):
@@ -117,6 +113,12 @@ class FeedItem():
                 break
             number = number + 1
             self.sendItem(feed, number)
+            if hasattr(feed, 'link'):
+                self.lastMD5 = getMD5(feed.title + feed.link)
+            else:
+                self.lastMD5 = getMD5(feed.title)
+            self.node.set('lastMD5', self.lastMD5)
+            self.saveXML()
 
         if hasLastMD5:
             for feed in reversed(self.feedD.entries):
@@ -124,20 +126,47 @@ class FeedItem():
                     break
                 number = number + 1
                 self.sendItem(feed, number)
+            if hasattr(feed, 'link'):
+                self.lastMD5 = getMD5(feed.title + feed.link)
+            else:
+                self.lastMD5 = getMD5(feed.title)
+            self.node.set('lastMD5', self.lastMD5)
+            self.saveXML()
 
-        feed = self.feedD.entries[0]
-        if hasattr(feed, 'link'):
-            self.lastMD5 = getMD5(feed.title + feed.link)
+        if hasattr(self.feedD, 'modified'):
+            self.lastModified = self.feedD.modified
         else:
-            self.lastMD5 = getMD5(feed.title)
-        self.saveStatus()
+            self.lastModified = ""
+        self.node.set('lastModified', self.lastModified)
+        self.saveXML()
 
-        if number == 0:
-            logging.info("No Update.")
-            return False
+        logging.info("Updated %s items.", str(number))
+        return True
+
+def dfs_rss(num, root, tot, session, sender, recipient, rsslist):
+    for item in root:
+        if item.tag == 'body':
+            continue
+        if 'type' in item.attrib.keys():
+            if root.tag != 'body':
+                recipient_ = (recipient.split('@')[0] + 
+                                '+' + root.attrib['text'] + 
+                                '@' + recipient.split('@')[1])
+            else:
+                recipient_ = recipient
+            num = num + 1
+            outputStep = '(' + str(num) + '/' + str(tot) + ')'
+            feedItem = FeedItem(item, outputStep, session, sender, recipient_, rsslist)
+            ret = feedItem.update()
         else:
-            logging.info("Updated %s items.", str(number))
-            return True
+            if len(item.attrib['text']) != len(item.attrib['text'].encode('utf-8')):
+                logging.error('The tag name can''t be Chinese')
+                item.clear()
+                continue
+            num = dfs_rss(num, item, tot, session, sender, recipient, rsslist)
+            item.clear()
+
+    return num
 
 def init():
     reload(sys)
@@ -148,11 +177,6 @@ def init():
         logging.error('no rsslist file')
         sys.exit()
 
-    if os.path.isfile('rsslist.xml.swp'):
-        logging.warning('Last run is failed, recovering...')
-        shutil.copy('rsslist.xml.swp', 'rsslist.xml')
-    else: 
-        shutil.copy('rsslist.xml', 'rsslist.xml.swp')
     logging.info('rss2email is running...')
 
 def loginEmail():
@@ -183,35 +207,6 @@ def getList():
 
     return tree 
 
-def dfs_rss(num, root, output, tot, session, sender, recipient):
-    for item in root:
-        if item.tag == 'body':
-            continue
-        if 'type' in item.attrib.keys():
-            if root.tag != 'body':
-                recipient_ = (recipient.split('@')[0] + 
-                                #'+' + item.attrib['text'] + 
-                                '+' + root.attrib['text'] + 
-                                '@' + recipient.split('@')[1])
-            else:
-                recipient_ = recipient
-            num = num + 1
-            outputStep = '(' + str(num) + '/' + str(tot) + ')'
-            node = ET.SubElement(output, 'item')
-            feedItem = FeedItem(item, node, session, outputStep, sender, recipient_)
-            ret = feedItem.update()
-        else:
-            if len(item.attrib['text']) != len(item.attrib['text'].encode('utf-8')):
-                logging.error('The tag name can''t be Chinese')
-                item.clear()
-                continue
-            node = ET.SubElement(output, 'tag')
-            node.set('text', item.attrib['text'])
-            num = dfs_rss(num, item, node, tot, session, sender, recipient)
-            item.clear()
-
-    return num
-
 def destory(session, root_output):
     session.quit()
     
@@ -221,9 +216,6 @@ def destory(session, root_output):
     o_file.write(doc.toprettyxml())
     o_file.close()
 
-    os.remove('rsslist.xml.swp')
-    
-
 def main():
     init()
     session, sender, recipient = loginEmail()
@@ -232,13 +224,8 @@ def main():
     tot = 0
     for i in rsslist.iter(tag='item'):
         tot = tot +1
-    root_output = ET.Element('root')
-    head_output = ET.SubElement(root_output, 'head')
-    title = ET.SubElement(head_output, 'title')
-    title.text = 'subscriptions for rss2email'
-    body_output = ET.SubElement(root_output, 'body')
 
-    dfs_rss(0, rsslist.find('body'), body_output, tot, session, sender, recipient)
+    dfs_rss(0, rsslist.find('body'), tot, session, sender, recipient, rsslist)
 
     destory(session, root_output)
 
